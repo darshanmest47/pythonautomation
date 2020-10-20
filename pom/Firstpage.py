e1f0e3f4ab4770ecc2fa90fb62b875e530179dcc
surajmesta <surajmesta47@gmail.com>
2020-10-20 09:32:20 +0530

    modified commit

--- pom/Firstpage.py
+++ pom/Firstpage.py
@@ -8,15 +8,14 @@ from utilities.Baseclass import Baseclass
 
 
 class Firstpage:
-    projects=(By.XPATH,"//ul[@class='nav-ul']/li[1]")
-    contact=(By.XPATH,"//ul[@class='nav-ul']/li[2]")
-    title=(By.XPATH,"//span[@class='sp1']")
-    react=(By.XPATH,"//h4[contains(text(),'React')]")
-    link1=(By.XPATH,"//div[@class='pro1-div']/div[@class='butt-div']/button[3]/a")
+    projects = (By.XPATH, "//ul[@class='nav-ul']/li[1]")
+    contact = (By.XPATH, "//ul[@class='nav-ul']/li[2]")
+    title = (By.XPATH, "//span[@class='sp1']")
+    react = (By.XPATH, "//h4[contains(text(),'React')]")
+    link1 = (By.XPATH, "//div[@class='pro1-div']/div[@class='butt-div']/button[3]/a")
 
-
-    def __init__(self,driver):
-        self.driver=driver
+    def __init__(self, driver):
+        self.driver = driver
 
     def scrolltotop(self):
         time.sleep(2)
@@ -24,32 +23,30 @@ class Firstpage:
 
     def mouseover1(self):
 
-        acts=ActionChains(self.driver)
-        pele=self.driver.find_element(*Firstpage.projects)
+        acts = ActionChains(self.driver)
+        pele = self.driver.find_element(*Firstpage.projects)
 
-
-        bs=Baseclass()
-        bs.movetoele(acts,pele)
+        bs = Baseclass()
+        bs.movetoele(acts, pele)
 
     def mouseover2(self):
-        acts2=ActionChains(self.driver)
-        pele2=self.driver.find_element(*Firstpage.contact)
+        acts2 = ActionChains(self.driver)
+        pele2 = self.driver.find_element(*Firstpage.contact)
         bs2 = Baseclass()
         bs2.movetoele(acts2, pele2)
 
     def switchToFrame1(self):
         self.driver.execute_script("window.scrollTo(0,600)")
         self.driver.switch_to.frame(0)
-        assert self.driver.find_element(*Firstpage.title).text == "Covid19-Tracker-India"
-        amt=1
-        amt2=1
-        for ele in range(1,8):
-
-            amt=amt*2
+        # assert self.driver.find_element(*Firstpage.title).text == "Covid19-Tracker-India"
+        amt = 1
+        amt2 = 1
+        for ele in range(1, 8):
+            amt = amt * 2
             time.sleep(1)
             self.driver.execute_script(f"window.scrollBy(0,{amt})")
 
-        for ele1 in range(1,8):
+        for ele1 in range(1, 8):
             amt2 = amt2 * 2
             time.sleep(1)
             self.driver.execute_script(f"window.scrollBy(0,{-amt2})")
@@ -62,7 +59,7 @@ class Firstpage:
         self.driver.execute_script("window.scrollTo(0,1090)")
         self.driver.switch_to.frame(1)
 
-        assert self.driver.find_element(*Firstpage.react).text =="React-Quiz"
+        assert self.driver.find_element(*Firstpage.react).text == "React-Quiz"
         amt = 1
         amt2 = 1
         for ele in range(1, 8):
@@ -81,21 +78,16 @@ class Firstpage:
         time.sleep(1)
         self.driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
         time.sleep(1)
-        pros=self.driver.find_element(*Firstpage.projects)
-        self.driver.execute_script("arguments[0].scrollIntoView();",pros)
-        
-    
+        pros = self.driver.find_element(*Firstpage.projects)
+        self.driver.execute_script("arguments[0].scrollIntoView();", pros)
 
     def openpro1(self):
         time.sleep(1)
         self.driver.find_element(*Firstpage.link1).click()
-        windows=self.driver.window_handles
-
-
-
+        windows = self.driver.window_handles
 
         self.driver.switch_to.window(windows[1])
-        cvd=Covidtracker(self.driver)
+        cvd = Covidtracker(self.driver)
         cvd.clicksel()
         cvd.selval()
 
@@ -103,22 +95,5 @@ class Firstpage:
             print(w)
             self.driver.switch_to.window(w)
             print(self.driver.title)
-            if self.driver.title=='CovidtrackerIndia':
+            if self.driver.title == 'CovidtrackerIndia':
                 self.driver.close()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
